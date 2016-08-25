64a42d55d6c505e54becadcd160563b127b568e8
Michael Berlin <mberlin@google.com>
2016-08-25 00:44:53 -0700

    WIP: throttler: Update last time the rate was changed only if the rate actually changed.
    TODO(mberlin): Write a test for this.

--- go/vt/throttler/max_replication_lag_module.go
+++ go/vt/throttler/max_replication_lag_module.go
@@ -70,9 +70,8 @@ type MaxReplicationLagModule struct {
 	rate         sync2.AtomicInt64
 	currentState state
 	// lastRateChange is the time when rate was adjusted last.
-	lastRateChange       time.Time
-	lastRateChangeReason string
-	nextAllowedIncrease  time.Time
+	lastRateChange      time.Time
+	nextAllowedIncrease time.Time
 	// replicaUnderIncreaseTest holds the discovery.TabletStats.Key value for the
 	// replica for which we started the last increase test. After
 	// nextAllowedIncrease is up, we wait for a lag update from this replica.
@@ -543,8 +542,6 @@ func (m *MaxReplicationLagModule) updateRate(r *result, newState state, rate int
 	oldRate := m.rate.Get()
 
 	m.currentState = newState
-	m.lastRateChange = now
-	m.lastRateChangeReason = reason
 	if newState != stateIncreaseRate {
 		// Clear the replica under test from a previous increase.
 		m.replicaUnderIncreaseTest = ""
@@ -564,6 +561,7 @@ func (m *MaxReplicationLagModule) updateRate(r *result, newState state, rate int
 		return
 	}
 
+	m.lastRateChange = now
 	m.rate.Set(int64(rate))
 	// Notify the throttler that we updated our max rate.
 	m.rateUpdateChan <- struct{}{}
